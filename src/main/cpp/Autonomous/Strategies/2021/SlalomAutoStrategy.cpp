2a9dbca34a9001fad97538c60370dbfa0a2a8d44
Dhairya Dodia <dhairyadodia2024@gmail.com>
2021-01-26 19:01:08 -0600

    test stage

--- src/main/cpp/Autonomous/Strategies/2021/SlalomAutoStrategy.cpp
+++ src/main/cpp/Autonomous/Strategies/2021/SlalomAutoStrategy.cpp
@@ -19,7 +19,7 @@ void SlalomAutoStrategy::Init(std::shared_ptr<World> world) {
 	steps.push_back(new OpenDriveToDistance(angle, 0.5, 0.0, 40.7, 1, 0.4));
 	// steps.push_back(new DriveToDistance(angle, speed, 0_in, 40.7_in));
 	steps.push_back(new DriveToDistance(angle, speed, -62_in, 36_in));
-	 steps.push_back(new DriveToDistance(angle, speed, 0_in, 140.7_in));
+	steps.push_back(new DriveToDistance(angle, speed, 0_in, 140.7_in));
 	//steps.push_back(new OpenDriveToDistance(angle, 0.6, 0.0, 121, 2, 0.25, 30));
 	steps.push_back(new DriveToDistance(angle, speed, 62_in, 36_in));
 	steps.push_back(new DriveToDistance(angle, speed, 0_in, 54.3_in));
