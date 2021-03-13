fb59de8f1675b68f3feebdf5cc9ba5cd6c0debe9
Jason Smith <devjason@gmail.com>
2021-03-13 16:27:27 -0600

    localmap

--- src/main/include/Sensors/LocalMap.h
+++ src/main/include/Sensors/LocalMap.h
@@ -23,12 +23,12 @@ public:
     {
         // update map x,y
         double yaw = RobotMap::gyro->GetYaw();
-        inch_t curx = encoderWheel->GetX();
-        inch_t cury = encoderWheel->GetY();
+        double curx = encoderWheel->GetX().to<double>();
+        double cury = encoderWheel->GetY().to<double>();
         
         double theta = yaw * DEG_TO_RAD;
-        inch_t diffx = curx - field_x;
-        inch_t diffy = cury - field_y;
+        double diffx = curx - field_x;
+        double diffy = cury - field_y;
 
         if (false) {
             // dumb ignoring angle
@@ -40,8 +40,8 @@ public:
              * x' = x cos(t) - y sin(t)
              * y' = x sin(t) + y cos(t)
              */
-            double x = diffx.to<double>();
-            double y = diffy.to<double>();
+            double x = diffx;
+            double y = diffy;
             double actual_x = (x * cos(theta)) - (y * sin(theta));
             double actual_y = (x * sin(theta)) + (y * cos(theta));
 
