fe4fa80931f5ec457538358398dcd3fcc55c660a
FRC Team 16 <frcteam16@gmail.com>
2021-03-24 18:47:16 -0500

    Update BarrelRaceAutoStrategy.cpp

--- src/main/cpp/Autonomous/Strategies/2021/BarrelRaceAutoStrategy.cpp
+++ src/main/cpp/Autonomous/Strategies/2021/BarrelRaceAutoStrategy.cpp
@@ -13,17 +13,21 @@ void BarrelRaceAutoStrategy::Init(std::shared_ptr<World> world) {
 	const double speed = 0.50;
 	const double angle = 0.0;
 
-	steps.push_back(new DriveToDistance(angle,speed,0_in,105_in));
-	steps.push_back(new DriveToDistance(angle,speed,30_in,0_in));
+	steps.push_back(new DriveToDistance(angle,speed,0_in,150_in));
+
+	// Going around first point
+	steps.push_back(new DriveToDistance(angle,speed,50_in,0_in));
+	steps.push_back(new DriveToDistance(angle,speed,0_in,-45_in));
+	steps.push_back(new DriveToDistance(angle,speed,-50_in,0_in));
+
+
+	steps.push_back(new DriveToDistance(angle,speed,-60_in,150_in));
+	steps.push_back(new DriveToDistance(angle,speed,-35_in,0_in));
 	steps.push_back(new DriveToDistance(angle,speed,0_in,-40_in));
-	steps.push_back(new DriveToDistance(angle,speed,-30_in,0_in));
-	steps.push_back(new DriveToDistance(angle,speed,-35_in,190_in));
-	steps.push_back(new DriveToDistance(angle,speed,-30_in,0_in));
-	steps.push_back(new DriveToDistance(angle,speed,0_in,-40_in));
-	steps.push_back(new DriveToDistance(angle,speed,30_in,0_in));
-	steps.push_back(new DriveToDistance(angle,speed,108_in,75_in));
-	steps.push_back(new DriveToDistance(angle,speed,-30_in,0_in));
-	steps.push_back(new OpenDriveToDistance(angle,0.7,0,-240,1,0.4));
+	steps.push_back(new DriveToDistance(angle,speed,35_in,0_in));
+	steps.push_back(new DriveToDistance(angle,speed,90_in,90_in));
+	steps.push_back(new DriveToDistance(angle,speed,-35_in,0_in));
+	steps.push_back(new OpenDriveToDistance(angle,-0.7,0,240,1,0.4));
 
   
 	//steps.push_back(new OpenDriveToDistance(angle, 0.5, 0.0, 41.5, 1, 0.4));
