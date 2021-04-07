d197ea86ffcf2eebe0c62ee35e111fc39b3578b9
FRC Team 16 <frcteam16@gmail.com>
2021-04-06 21:22:26 -0500

    Barrel 11s

--- src/main/cpp/Autonomous/Strategies/2021/BarrelRaceAutoStrategy.cpp
+++ src/main/cpp/Autonomous/Strategies/2021/BarrelRaceAutoStrategy.cpp
@@ -10,31 +10,31 @@ BarrelRaceAutoStrategy::BarrelRaceAutoStrategy(std::shared_ptr<World> world) {
 
 void BarrelRaceAutoStrategy::Init(std::shared_ptr<World> world) {
 	std::cout << "BarrelRaceAutoStrategy::Init()\n";
-	const double speed = 0.65;
+	const double speed = 0.7;
 	const double angle = 0.0;
 
 	steps.push_back(new DriveToDistance(angle,0.7,0_in,155_in));
 
 	// Going around first point
-	steps.push_back(new DriveToDistance(angle,speed,45_in,0_in));
+	steps.push_back(new DriveToDistance(angle,speed,50_in,0_in));
 	steps.push_back(new DriveToDistance(angle,speed,0_in,-50_in));
 	steps.push_back(new DriveToDistance(angle,speed,-40_in,0_in));
 
 	// Going around second point
-	steps.push_back(new DriveToDistance(angle,0.7,-28_in,180_in)); 
-	steps.push_back(new DriveToDistance(angle,speed,-35_in,0_in));
-	steps.push_back(new DriveToDistance(angle,speed,0_in,-55_in));
-	steps.push_back(new DriveToDistance(angle,speed,50_in,0_in));
+	steps.push_back(new DriveToDistance(angle,0.8,-23_in,175_in)); 
+	steps.push_back(new DriveToDistance(angle,speed,-48_in,0_in));
+	steps.push_back(new DriveToDistance(angle,speed,0_in,-50_in));
+	steps.push_back(new DriveToDistance(angle,speed,30_in,0_in));
 
 	// Going around third point
-	steps.push_back(new DriveToDistance(angle,0.7,65_in,90_in));
-	steps.push_back(new DriveToDistance(angle,speed,0_in, 40_in));
-	steps.push_back(new DriveToDistance(angle,speed,-60_in,0_in));
-	steps.push_back(new OpenDriveToDistance(angle,-0.75,0,240,1,0.4));
+	steps.push_back(new DriveToDistance(angle,0.8,80_in,80_in));
+	steps.push_back(new DriveToDistance(angle,speed,0_in, 55_in));
+	steps.push_back(new DriveToDistance(angle,speed,-53_in,0_in));
+	steps.push_back(new OpenDriveToDistance(angle,-0.95,0,240,1,0.4));
 
 
 	// Brake
-	steps.push_back(new DriveToDistance(angle, 0.3, 0_in, 5_in));
+	steps.push_back(new DriveToDistance(angle, 0.5, 0_in, 10_in));
 	steps.push_back(new DriveToDistance(angle, 0.0, 0_in, 0_in)); 
 
   
